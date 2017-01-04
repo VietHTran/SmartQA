8eee1d8883f036576f35edb8a21b225e8278b9c0
Viet Hung Tran <trantechenterprise@gmail.com>
2017-01-04 13:09:47 -0500

    Enable Ignore Code Blocks Text

--- SmartQA/src/main/java/com/viet/SmartQA/SQA.java
+++ SmartQA/src/main/java/com/viet/SmartQA/SQA.java
@@ -22,13 +22,15 @@ public class SQA
 	private static String[] lvl1Words; //Lose points
 	private static String[] lvl2Words; //Immediately exit
 	private static String[] question;
-	
+	private static boolean isCode=false;
+	private static boolean isQuote=false;
     public static void main( String[] args ) throws IOException
     {
     	englishWords=getStringFromFiles(WORDS_LIST_PATH);
     	lvl1Words=getStringFromFiles(LEVEL1_PATH);
     	lvl2Words=getStringFromFiles(LEVEL2_PATH);
     	int points=30;
+    	
         try {
         	question=getStringFromFiles("TextFiles/text.txt");
         }catch (NoSuchFileException e) {
@@ -37,9 +39,15 @@ public class SQA
         }
         
         for (int i=0;i<question.length;i++) {
-        	String[] words = question[i].split(" ");
+        	String[] words = question[i].split("\\s+");
+        	if (words==null || words.length==0) continue;
         	for (int k=0;k<words.length;k++) {
-        		if (isNumeric(words[k])) continue;
+        		checkBlockOfCode(words[k]);
+        		if (words[k]==null 
+        				|| words[k].equals("")
+        				||isNumeric(words[k]) 
+        				||isCodeLine(words[k]) 
+        				||isTag(words[k])) continue;
         		//Create a copy of word and leave out all none-alphabetic characters
         		String holder=words[k].toLowerCase()
         				.replaceAll("[^a-z]", "");
@@ -48,6 +56,8 @@ public class SQA
         			System.out.println("Rated: RTFM");
         			return;
         		}
+        		//no need to check for grammar and inappropriate words in code
+        		if (isCode) continue;
         		if (binarySearchWord(holder.toLowerCase(),lvl1Words)){
         			printStatus("Inappropriate words or characters",words[k],i,k);
         			points-=5;
@@ -63,6 +73,25 @@ public class SQA
         if (points<=10) System.out.println("Rated: RTFM");
         else if (points<=15) System.out.println("Rated: Average question");
         else System.out.println("Rated: Smart question");
+    }
+    
+    private static void checkBlockOfCode(String word) {
+    	if (word.equals("<code>") || word.equals("<pre>")) 
+    		isCode=true; //inside code block
+    	else if (word.equals("</code>") || word.equals("</pre>")) 
+    		isCode=false; //no longer inside code block
+    	//System.out.println(word+" "+isCode);
+    }
+    
+    private static boolean isCodeLine(String word) {
+    	//check if the word is marked as code text
+    	return (word.charAt(0)=='`' && word.charAt(word.length()-1)=='`');
+    }
+    
+    private static boolean isTag(String word) {
+    	//check if word is an html tag
+    	return ((word.charAt(0)=='<' && word.charAt(word.length()-1)=='>')||
+    			word.charAt(0)=='[' && word.charAt(word.length()-1)==')');
     }
     
     private static void printStatus(String statusType, String word, int x,int y) {

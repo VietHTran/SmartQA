079a3694031af27b8858095038ba5c5315d2f400
Viet Hung Tran <trantechenterprise@gmail.com>
2017-01-18 23:06:14 -0500

    Add Add New Word Feature

--- SmartQA/src/main/java/com/viet/SmartQA/SQA.java
+++ SmartQA/src/main/java/com/viet/SmartQA/SQA.java
@@ -1,13 +1,18 @@
 package com.viet.SmartQA;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.io.SequenceInputStream;
+import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
@@ -39,20 +44,24 @@ public class SQA
 	private static final String WORDS_LIST_PATH="TextFiles/wordlist.txt";
 	private static final String LEVEL1_PATH="TextFiles/InappropriateWordsLvl1.txt";
 	private static final String LEVEL2_PATH="TextFiles/InappropriateWordsLvl2.txt";
-	private static final String TEST_PATH="TextFiles/IntegrationTests/2.txt";
+	//private static final String TEST_LIST_PATH="TextFiles/DictionaryTest.txt"; //test only
+	//private static final String TEST_PATH="TextFiles/CommandLineTest.txt"; //test only
 	private static String filePath;
 	private static String[] englishWords; //If not found then lose points
 	private static String[] lvl1Words; //Lose points
 	private static String[] lvl2Words; //Immediately exit
+	private static String[] testWords;
 	private static Pattern backticks=Pattern.compile("\\`+");
 	private static Pattern hyperlink=Pattern.compile("\\[.+?\\][(].+?[)]");
 	public static int points=30;
     public static void main( String[] args ) throws IOException
     {
-    	handleCommands(args);
+//    	args=splitWhiteSpace(getStringFromFiles(TEST_PATH)[0]); //test only
+//    	testWords=getStringFromFiles(TEST_LIST_PATH); //test only
     	englishWords=getStringFromFiles(WORDS_LIST_PATH);
     	lvl1Words=getStringFromFiles(LEVEL1_PATH);
     	lvl2Words=getStringFromFiles(LEVEL2_PATH);
+    	handleCommands(args);
     	String fileContent;
         try {
         	fileContent=htmlFormatToText(filePath);
@@ -71,19 +80,19 @@ public class SQA
     		//Create a copy of word and leave out all none-alphabetic characters
     		String holder=getAlphabetOnly(words[k]);
     		//System.out.println("before after0: "+words[k]+" "+holder); //debug
-    		if (binarySearchWord(holder,lvl2Words)){
+    		if (binarySearchWord(holder,lvl2Words)>-1){
     			printStatus("Rude words or characters",words[k]);
     			System.out.println("Rated: RTFM");
     			return;
     		}
     		//System.out.println("before after1: "+words[k]+" "+holder); //debug
-    		if (binarySearchWord(holder,lvl1Words)){
+    		if (binarySearchWord(holder,lvl1Words)>-1){
     			printStatus("Inappropriate words or characters",words[k]);
     			points-=5;
     			continue;
     		}
     		//System.out.println("before after2: "+words[k]+" "+holder); //debug
-    		if (!binarySearchWord(holder,englishWords)
+    		if (binarySearchWord(holder,englishWords)<=-1
     				&& holder.length()>=2 ){
     			printStatus("Grammar error",words[k]);
     			points--;
@@ -92,17 +101,104 @@ public class SQA
     	printResult();
     }
     
-    private static void handleCommands(String[] args) {
+    private static void handleCommands(String[] args) throws IOException {
     	int length=args.length;
     	if (args==null || length==0) exitCommand("No argument found for this command");
     	String command=args[0];
     	if (command.equals("check")) {
-    		if (length>2) exitCommand("Redundant argument");
-    		if (length==1) exitCommand("Use \"check [file path]\" command to check the appropriate level of the question");
+    		checkArguments(args,2,"Use \"check <file path>\" command to check the appropriate level of the question");
     		filePath=args[1];
     		if (!filePath.substring(length-4, length).equals(".txt")) exitCommand("Please type in path to file in .txt format");
     		return;
+    	} else if (command.equals("help")) {
+    		userGuide();
+    		exitCommand("");
+    	} else if (command.equals("add")) {
+    		checkArguments(args,3,"Use \"add <dictionary code> <word>\" command to add new word to selected dictionary");
+    		int dictCode=0;
+    		try {
+    			dictCode=Integer.parseInt(args[1]);
+    		} catch (NumberFormatException e) {
+    			exitCommand("Invalid dictionary code");
+    		}
+    		//if (dictCode>2 || dictCode<0) exitCommand("Invalid dictionary code");
+    		if (dictCode>3 || dictCode<0) exitCommand("Invalid dictionary code"); //test only
+    		String[] selectedDictionary;
+    		String dictionaryName;
+    		String path;
+    		String newWord=getAlphabetOnly(args[2]);
+			//System.out.println("Commands: "+args[0]+" "+args[1]+" "+args[2]+"\n"); //debug
+    		switch (dictCode) {
+    			case 1:
+    				selectedDictionary=lvl1Words;
+    				path=LEVEL1_PATH;
+    				dictionaryName="Inappropriate words list level 1";
+    				break;
+    			case 2:
+    				selectedDictionary=lvl2Words;
+    				path=LEVEL2_PATH;
+    				dictionaryName="Inappropriate words list level 2";
+    				break;
+//    			case 3: //test only
+//    				selectedDictionary=testWords;
+//    				path=TEST_LIST_PATH;
+//    				dictionaryName="Test dictionary";
+//    				break;
+    			default:
+    				selectedDictionary=englishWords;
+    				path=WORDS_LIST_PATH;
+    				dictionaryName="English dictionary";
+    		}
+    		int index=binarySearchWord(newWord,selectedDictionary);
+    		if (index>-1) {
+    			exitCommand(dictionaryName+" already contains word \""+args[2]+"\"");
+    		} else {
+    			index*=-1;
+    			index+=newWord.compareTo(selectedDictionary[index])<0 ? 1:2; //file is base 1
+    			//System.out.println(index+": "+selectedDictionary[index-1]); //debug
+    			try {
+    				insertStringInFile(new File(path),index, newWord);
+    			} catch (Exception e) {
+    				e.printStackTrace();
+    				exitCommand("");
+    			}
+    			exitCommand("New word \""+newWord+"\" is successfully inserted into "+dictionaryName);
+    		}
     	}
+    }
+    
+	private static void insertStringInFile(File inFile, int line, String lineToBeInserted) throws Exception {
+		File outFile = new File("$$$$$$$$.txt");
+		FileInputStream fis = new FileInputStream(inFile);
+		BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+		FileOutputStream fos = new FileOutputStream(outFile);
+		PrintWriter out = new PrintWriter(fos);
+		String thisLine = "";
+		int i = 1;
+		while ((thisLine = in.readLine()) != null) {
+			if (i == line)
+				out.println(lineToBeInserted);
+			out.println(thisLine);
+			i++;
+		}
+		out.flush();
+		out.close();
+		in.close();
+		inFile.delete();
+		outFile.renameTo(inFile);
+	}
+    
+    private static void checkArguments(String[] args, int requiredArgs, String description) {
+    	int length=args.length;
+    	if (length>requiredArgs) exitCommand("Redundant argument");
+    	if (length<requiredArgs) exitCommand(description);
+    }
+    
+    private static void userGuide() {
+    	System.out.println("check <txt path>: check the appropriate level of the question in the text file");
+    	System.out.println("add <dictionary code> <word>: add new word to selected dictionary");
+    	//System.out.println("remove <dictionary code> <word>: remove a word out of the selected dictionary");
+    	System.out.println("\tdictionary code: english dictionary[0], inappropriate words level 1[1], inappropriate words level 2[2]");
     }
     
     private static void exitCommand(String respond) {
@@ -252,23 +348,23 @@ public class SQA
     	return content;
     }
     
-    private static boolean binarySearchWord(String word, String[] collections) {
+    private static int binarySearchWord(String word, String[] collections) {
     	if (word==null || word.length()<2 ||
-    			collections==null || collections.length==0) return false;
-    	int left=0,right=collections.length-1,mid;
+    			collections==null || collections.length==0) return -1;
+    	int left=0,right=collections.length-1,mid=1;
     	//System.out.println(word);
     	while (left<=right) {
-    		mid=(left+right)/2;
+    		mid=(left+right)>>1;
     		//System.out.println("mid word: "+collections[mid]);
     		int comp=word.compareTo(collections[mid]);
     		if (comp==0)
-    			return true;
+    			return mid;
     		else if (comp>0) //word is lexicographically larger than collections[i]
     			left=mid+1;
     		else //word is lexicographically smaller than collections[i]
     			right=mid-1;
     	}
-    	return false;
+    	return -1*mid;
     }
     
     private static boolean isNumeric(String s) {  
